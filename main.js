bcbced8f365bf55dad74f7d0481004f48e846e5a
wfeliciano20 <william.feliciano2@gmail.com>
2023-08-09 19:53:23 -0400

    L4-done Conditional Rendering

--- main.js
+++ main.js
@@ -4,6 +4,9 @@ const app = Vue.createApp({
             product: "Socks",
             description: "Wonderful wool socks",
             image: "./assets/images/socks_green.jpg",
-            url: "https://google.com"
-    }}
+            url: "https://google.com",
+            inStock: true,
+            stockNumber: 1,
+        }
+    }
 });
