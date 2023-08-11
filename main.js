1f902d7939f8d6a5186c020a9b6ffb7b1657e059
wfeliciano20 <william.feliciano2@gmail.com>
2023-08-10 20:39:28 -0400

    L5-done List rendering

--- main.js
+++ main.js
@@ -7,6 +7,12 @@ const app = Vue.createApp({
             url: "https://google.com",
             inStock: true,
             stockNumber: 1,
+            details: ["50% cotton", "30% wool", "20% polyester"],
+            variants: [
+                { id: 2234, color: "green" },
+                { id: 2235, color: "blue" }
+            ],
+            sizes: ["S", "M", "L", "XL", "XXL", "XXXL"],
         }
     }
 });
