d07c148e6788a4a252f6758565d62a92caf048c7
wfeliciano20 <william.feliciano2@gmail.com>
2023-08-10 23:15:23 -0400

    L8-done Computed Properties

--- main.js
+++ main.js
@@ -2,16 +2,16 @@ const app = Vue.createApp({
     data() {
         return {
             cart: 0,
+            brand: "Vue Mastery",
             product: "Socks",
+            onSale: true,
             description: "Wonderful wool socks",
-            image: "./assets/images/socks_green.jpg",
             url: "https://google.com",
-            inStock: true,
-            stockNumber: 1,
             details: ["50% cotton", "30% wool", "20% polyester"],
+            selectedVariant: 0,
             variants: [
-                { id: 2234, color: "green", image: "./assets/images/socks_green.jpg" },
-                { id: 2235, color: "blue", image: "./assets/images/socks_blue.jpg" },
+                { id: 2234, color: "green", image: "./assets/images/socks_green.jpg", quantity: 50 },
+                { id: 2235, color: "blue", image: "./assets/images/socks_blue.jpg", quantity: 0 },
             ],
             sizes: ["S", "M", "L", "XL", "XXL", "XXXL"],
         }
@@ -20,20 +20,35 @@ const app = Vue.createApp({
         addToCart() {
             this.cart += 1
             this.stockNumber -= 1
-            if (this.stockNumber == 0) {
-                this.inStock = false
-            }
         },
         updateImage(variantImage) {
             this.image = variantImage
         },
         removeFromCart() {
-
             this.cart -= 1
             this.stockNumber += 1
-            if (this.stockNumber > 0) {
-                this.inStock = true
-            }
+        },
+        updateVariant(index) {
+            this.selectedVariant = index
         }
+    },
+    computed: {
+        title() {
+            return this.brand + ' ' + this.product
+        },
+        image() {
+            return this.variants[this.selectedVariant].image
+        },
+        inStock() {
+            return this.variants[this.selectedVariant].quantity > 0
+        },
+        stockNumber() {
+            return this.variants[this.selectedVariant].quantity
+        },
+        sale() {
+            return this.brand + ' ' + this.product + ' are on sale!'
+        }
+
+
     }
 });
